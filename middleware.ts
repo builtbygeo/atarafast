9a1be9bb7367e04f5b1bb61bee5cf66e84b8ec69
bemorebased <gag1000x@icloud.com>
2026-03-05 20:12:32 +0200

    fix: restore middleware protection

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isAppRoute = createRouteMatcher(['/app(.*)'])
 
-export default clerkMiddleware()
+export default clerkMiddleware(async (auth, req) => {
+    if (isAppRoute(req)) {
+        await auth.protect()
+    }
+})
 
 export const config = {
     matcher: [
